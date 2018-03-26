d651e6c441f51de13da83e51f2575eab030d9ded
rorycamw <Rory>
2018-03-26 10:25:32 -0700

    Compatible with Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 8080;
+
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -48,6 +50,6 @@ app.get('/404', (request, response) => {
 	})
 })
 
-app.listen(8080, () => {
-	console.log('Server is up on the port 8080');
+app.listen(port, () => {
+	console.log('Server is up on the port ${port}');
 });
